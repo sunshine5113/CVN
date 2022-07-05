a1ef748b98e8978274d0b3f689254fcb28214322
sunshine5113 <fullstack.sunshine@gmail.com>
2022-07-05 16:42:08 +0900

    update: 7-0501

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 100, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});
+
+
+luxy.init();
